ad5f75126e1ef7dce95609dba39230b070b307cd
Your Name <you@example.com>
2024-09-13 14:00:33 +0800

    commit

--- src/Search.js
+++ src/Search.js
@@ -39,26 +39,27 @@ const Search = () => {
                         <button className="btn btn-primary" type="submit" onClick={handleSearch}>Search</button>
                     </span>
                 </div>
+                
+                <div className="row row-cols-1 row-cols-md-3 g-4">           
+                        {games.map((gamelist) => {
+                            return (
+                                <div className="col" key={gamelist.id}>
+                                <div className="card h-100">
+                                    <img src={gamelist.thumb} className="card-img-top" alt=""/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{gamelist.external}</h5>
+                                        <p className="card-text">{gamelist.cheapest}</p>
+                                    </div>
+                                    <div className="card-footer">
+                                        <small className="text-muted">Release Date: {gamelist.cheapest}</small>
+                                    </div>
+                                </div>
+                                </div>
+                            )
+                        })}
+                    </div>    
             </form>
         </div>
-        <div className="row row-cols-1 row-cols-md-3 g-4">           
-                {games.map((gamelist) => {
-                    return (
-                        <div className="col" key={gamelist.id}>
-                        <div className="card h-100">
-                            <img src={gamelist.thumb} className="card-img-top" alt=""/>
-                            <div className="card-body">
-                                <h5 className="card-title">{gamelist.external}</h5>
-                                <p className="card-text">{gamelist.cheapest}</p>
-                            </div>
-                            <div className="card-footer">
-                                <small className="text-muted">Release Date: {gamelist.cheapest}</small>
-                            </div>
-                        </div>
-                        </div>
-                    )
-                })}
-            </div>    
     </section>
     </>
   )
