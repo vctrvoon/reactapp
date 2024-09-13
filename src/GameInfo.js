c888e389f50c87c78fd351d985cfb6eb7461230a
Your Name <you@example.com>
2024-09-13 16:13:04 +0800

    commit

--- src/GameInfo.js
+++ src/GameInfo.js
@@ -43,7 +43,7 @@ const GameInfo = () => {
 
       {/* Section Title */}
       <div className="container section-title" data-aos="fade-up">
-        <h2>fsafdsdf</h2>
+        <h2>{gameDetails.name}</h2>
         <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
       </div>{/* End Section Title */}
 
