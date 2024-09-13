05dd2d149eced4f9f1379cb7cbde5ca8a8006aec
Your Name <you@example.com>
2024-09-13 14:31:13 +0800

    commit

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 import axios from 'axios'
 
-   const searchTitleURL = 'https://www.cheapshark.com/api/1.0/games?title=batman'
+   const searchTitleURL = 'https://www.cheapshark.com/api/1.0/games?title='
 
 const Search = () => { 
     
@@ -44,18 +44,16 @@ const Search = () => {
                         {games.map((gamelist) => {
                             return (
                                 <div className="col" key={gamelist.id}>
-                                <div className="card h-100">
-                                    <div width="414" height="551" >
+                                    <div className="card h-100">
                                         <img src={gamelist.thumb} className="card-img-top" alt=""/>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{gamelist.external}</h5>
+                                            <p className="card-text">{gamelist.cheapest}</p>
+                                        </div>
+                                        <div className="card-footer">
+                                            <small className="text-muted">Release Date: {gamelist.cheapest}</small>
+                                        </div>
                                     </div>
-                                    <div className="card-body">
-                                        <h5 className="card-title">{gamelist.external}</h5>
-                                        <p className="card-text">{gamelist.cheapest}</p>
-                                    </div>
-                                    <div className="card-footer">
-                                        <small className="text-muted">Release Date: {gamelist.cheapest}</small>
-                                    </div>
-                                </div>
                                 </div>
                             )
                         })}
